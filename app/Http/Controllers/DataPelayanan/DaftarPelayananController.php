0b4de2cfc72a2af9f6c3f6c0b3b941e0c376a513
Pramana Yuda Sayeti <pramanayuda772@gmail.com>
2022-12-06 12:23:35 +0700

    push

--- app/Http/Controllers/DataPelayanan/DaftarPelayananController.php
+++ app/Http/Controllers/DataPelayanan/DaftarPelayananController.php
@@ -118,8 +118,8 @@ class DaftarPelayananController extends Controller
 
     public function create()
     {
-        $daftarLayanan = \App\Models\DaftarLayanan::all();
-        // $daftarLayanan = DaftarLayanan::whereHas('syarat')->get();
+        // $daftarLayanan = \App\Models\DaftarLayanan::all();
+        $daftarLayanan = DaftarLayanan::whereHas('syarat')->get();
         return view('admin.daftar-pelayanan.create', [
             'title'  => 'Input - Lacak Pelayanan Publik',
             'br1'  => 'Input - Lacak',
