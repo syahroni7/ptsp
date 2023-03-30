c75e5bbe10e87779bb3884d98a0fa28cc4167b97
Pramana Yuda Sayeti <pramanayuda772@gmail.com>
2023-03-30 13:29:35 +0700

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,6 +54,10 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        $access_type = DB::table('access_type')->first();
+        $type = $access_type->name;
+
+        $statusPelayanan = [];
         $statusPelayanan = [
             [
                 'name' => 'Baru',
@@ -78,7 +82,7 @@ class HomeController extends Controller
         ];
 
         // $pelayananColl = DaftarPelayanan::whereYear('created_at', '=', date('Y'))
-        //                                         ->whereMonth('created_at', '=', date('m'))
+            //                                         ->whereMonth('created_at', '=', date('m'))
         $pelayananColl = DaftarPelayanan::with('unit')
                                                 ->get();
 
@@ -89,191 +93,190 @@ class HomeController extends Controller
             $statusPelayanan[$key]['total'] = $countItem;
         }
 
-
-
         $sUnit = [];
-        $units = \App\Models\UnitPengolah::all();
-        foreach ($units as $key => $item) {
-            $sUnit[] = [
-                'name' => $item->name,
-                'full_name' => $item->name,
-                'value' => 0
-            ];
-        }
+        if ($type != 'MINIMAL') {
+            $units = \App\Models\UnitPengolah::all();
+            foreach ($units as $key => $item) {
+                $sUnit[] = [
+                    'name' => $item->name,
+                    'full_name' => $item->name,
+                    'value' => 0
+                ];
+            }
 
-        $pByUnit = $pelayananColl->groupBy('unit.name');
+            $pByUnit = $pelayananColl->groupBy('unit.name');
 
-        if ($pByUnit) {
-            foreach ($sUnit as $key => $item) {
-                $countItem = isset($pByUnit[$item['name']]) ? $pByUnit[$item['name']]->count() : 0;
-                $sUnit[$key]['value'] = $countItem;
+            if ($pByUnit) {
+                foreach ($sUnit as $key => $item) {
+                    $countItem = isset($pByUnit[$item['name']]) ? $pByUnit[$item['name']]->count() : 0;
+                    $sUnit[$key]['value'] = $countItem;
 
-                if (isset($pByUnit[$item['name']])) {
-                    $byStat = $pByUnit[$item['name']]->groupBy('status_pelayanan');
-                    $sUnit[$key]['Baru'] = isset($byStat['Baru']) ? $byStat['Baru']->count() : 0;
-                    $sUnit[$key]['Proses'] = isset($byStat['Proses']) ? $byStat['Proses']->count() : 0;
-                    $sUnit[$key]['Selesai'] = isset($byStat['Selesai']) ? $byStat['Selesai']->count() : 0;
-                } else {
-                    $sUnit[$key]['Baru'] = 0;
-                    $sUnit[$key]['Proses'] = 0;
-                    $sUnit[$key]['Selesai'] = 0;
-                }
+                    if (isset($pByUnit[$item['name']])) {
+                        $byStat = $pByUnit[$item['name']]->groupBy('status_pelayanan');
+                        $sUnit[$key]['Baru'] = isset($byStat['Baru']) ? $byStat['Baru']->count() : 0;
+                        $sUnit[$key]['Proses'] = isset($byStat['Proses']) ? $byStat['Proses']->count() : 0;
+                        $sUnit[$key]['Selesai'] = isset($byStat['Selesai']) ? $byStat['Selesai']->count() : 0;
+                    } else {
+                        $sUnit[$key]['Baru'] = 0;
+                        $sUnit[$key]['Proses'] = 0;
+                        $sUnit[$key]['Selesai'] = 0;
+                    }
 
-                if ($sUnit[$key]['name'] == 'Subbagian Tata Usaha') {
-                    $sUnit[$key]['name'] = 'SubbagTU';
-                }
+                    if ($sUnit[$key]['name'] == 'Subbagian Tata Usaha') {
+                        $sUnit[$key]['name'] = 'SubbagTU';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Pendidikan Madrasah') {
-                    $sUnit[$key]['name'] = 'Seksi PenMad';
-                }
+                    if ($sUnit[$key]['name'] == 'Seksi Pendidikan Madrasah') {
+                        $sUnit[$key]['name'] = 'Seksi PenMad';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Pendidikan Agama Islam') {
-                    $sUnit[$key]['name'] = 'Seksi PAIs';
-                }
+                    if ($sUnit[$key]['name'] == 'Seksi Pendidikan Agama Islam') {
+                        $sUnit[$key]['name'] = 'Seksi PAIs';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Pendidikan Diniyah dan Pondok Pesantren') {
-                    $sUnit[$key]['name'] = 'Seksi PD Pontren';
-                }
+                    if ($sUnit[$key]['name'] == 'Seksi Pendidikan Diniyah dan Pondok Pesantren') {
+                        $sUnit[$key]['name'] = 'Seksi PD Pontren';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Penyelenggaraan Haji dan Umrah') {
-                    $sUnit[$key]['name'] = 'Seksi PHU';
-                }
+                    if ($sUnit[$key]['name'] == 'Seksi Penyelenggaraan Haji dan Umrah') {
+                        $sUnit[$key]['name'] = 'Seksi PHU';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Bimbingan Masyarakat Islam') {
-                    $sUnit[$key]['name'] = 'Seksi BiMas';
-                }
+                    if ($sUnit[$key]['name'] == 'Seksi Bimbingan Masyarakat Islam') {
+                        $sUnit[$key]['name'] = 'Seksi BiMas';
+                    }
 
-                if ($sUnit[$key]['name'] == 'Seksi Penyelenggara Zakat dan Wakaf') {
-                    $sUnit[$key]['name'] = 'Seksi ZaWa';
+                    if ($sUnit[$key]['name'] == 'Seksi Penyelenggara Zakat dan Wakaf') {
+                        $sUnit[$key]['name'] = 'Seksi ZaWa';
+                    }
                 }
             }
         }
 
-        // return $sUnit;
+        // Berdasarkan Timeline
+        $dataDaily = null;
+        $dataWeekly = null;
+        if ($type != 'MINIMAL') {
+            // $total = TotalLayananPerMinggu::orderBy('id_total_layanan_perminggu', 'DESC')->take(8)->get();
+            $total = TotalLayananPerMinggu::orderBy('id_total_layanan_perminggu', 'DESC')->get();
+            $total = $total->sortBy("id_total_layanan_perminggu");
 
-        // $total = TotalLayananPerMinggu::orderBy('id_total_layanan_perminggu', 'DESC')->take(8)->get();
-        $total = TotalLayananPerMinggu::orderBy('id_total_layanan_perminggu', 'DESC')->get();
-        $total = $total->sortBy("id_total_layanan_perminggu");
+            $series [] = [
+                'name' => 'Total Pelayanan',
+                'data' => $total->pluck('total_pelayanan')
+            ];
 
-        $series [] = [
-            'name' => 'Total Pelayanan',
-            'data' => $total->pluck('total_pelayanan')
-        ];
+            // $categories = $total->pluck('week_range');
+            $categories = $total->pluck('week_end_date');
+            // return $categories;
 
-        // $categories = $total->pluck('week_range');
-        $categories = $total->pluck('week_end_date');
-        // return $categories;
+            $dataWeekly = [
+                'series' => $series,
+                'categories' => $categories
+            ];
 
-        $dataWeekly = [
-            'series' => $series,
-            'categories' => $categories
-        ];
+            $totalD = TotalLayananPerHari::get();
 
-        $totalD = TotalLayananPerHari::get();
+            $seriesD [] = [
+                'name' => 'Total Pelayanan',
+                'data' => $totalD->pluck('total_pelayanan')
+            ];
 
-        $seriesD [] = [
-            'name' => 'Total Pelayanan',
-            'data' => $totalD->pluck('total_pelayanan')
-        ];
+            $categoriesD = $totalD->pluck('date');
 
-        $categoriesD = $totalD->pluck('date');
+            $dataDaily = [
+                'series' => $seriesD,
+                'categories' => $categoriesD
+            ];
+        }
 
-        $dataDaily = [
-            'series' => $seriesD,
-            'categories' => $categoriesD
-        ];
+        // Tabel Rekapitulasi Bulanan
+        $daftarpelayanangrouped = null;
+        if ($type != 'MINIMAL') {
+            $daftarpelayanan = DB::select('
+                SELECT c.id_unit_pengolah as id_unit, c.name as unit, a.name as layanan, COALESCE(count(b.id_pelayanan)) as total
+                FROM daftar_layanan as a
+                LEFT JOIN daftar_pelayanan as b ON a.id_layanan = b.id_layanan
+                LEFT JOIN daftar_unit_pengolah as c ON a.id_unit_pengolah = c.id_unit_pengolah
+                GROUP BY c.id_unit_pengolah, c.name, a.name
+                ORDER BY c.id_unit_pengolah ASC');
+            $daftarpelayanangrouped = collect($daftarpelayanan)->groupBy('unit');
+        }
 
+        // Ringkasan Rekapitulasi
+        $fixData = null;
+        if ($type != 'MINIMAL') {
+            $pelayananS = DaftarPelayanan::orderBy('created_at', 'asc')->first();
+            $pelayananE = DaftarPelayanan::orderBy('created_at', 'desc')->first();
 
+            $start = $pelayananS->created_at;
+            $end = $pelayananE->created_at;
+            $range = $this->_getPeriodRange($start, $end);
 
-        $daftarpelayanan = DB::select('
-        SELECT c.id_unit_pengolah as id_unit, c.name as unit, a.name as layanan, COALESCE(count(b.id_pelayanan)) as total
-        FROM daftar_layanan as a
-        LEFT JOIN daftar_pelayanan as b ON a.id_layanan = b.id_layanan
-        LEFT JOIN daftar_unit_pengolah as c ON a.id_unit_pengolah = c.id_unit_pengolah
-        GROUP BY c.id_unit_pengolah, c.name, a.name
-        ORDER BY c.id_unit_pengolah ASC');
-        $daftarpelayanangrouped = collect($daftarpelayanan)->groupBy('unit');
-        // return $daftarpelayanangrouped;
-
-        // return $daftarpelayanan;
-
-        // Coba-coba
-
-        $pelayananS = DaftarPelayanan::orderBy('created_at', 'asc')->first();
-        $pelayananE = DaftarPelayanan::orderBy('created_at', 'desc')->first();
-
-        $start = $pelayananS->created_at;
-        $end = $pelayananE->created_at;
-        $range = $this->_getPeriodRange($start, $end);
-
-        $collData = [];
-        foreach ($range as $item) {
-            $getData = DB::select("
+            $collData = [];
+            foreach ($range as $item) {
+                $getData = DB::select("
             SELECT b.id_unit_pengolah, b.name, COALESCE(COUNT(a.id_pelayanan)) as total_layanan
             FROM daftar_unit_pengolah as b
             LEFT JOIN daftar_pelayanan as a ON b.id_unit_pengolah = a.id_unit_pengolah AND MONTH(a.created_at) = ? AND YEAR(a.created_at) = ?
             GROUP BY b.id_unit_pengolah, b.name
             ORDER BY b.id_unit_pengolah", [$item['month'], $item['year']]);
-            $collData[$item['title']] = $getData;
-        }
-
-        $fixData = [];
-        $fixData['header'][] = 'Nama Unit';
-
-
-
-        // return $collData;
-        $counter = 1;
-        foreach ($collData as $bulan => $item) {
-            $fixData['header'][] = $bulan;
-            foreach ($item as $k => $coll) {
-                if (!isset($fixData[$coll->name])) {
-                    $fixData[$coll->name][] = $coll->name;
-                    $fixData[$coll->name][] = $coll->total_layanan;
-                } else {
-                    $fixData[$coll->name][] = $coll->total_layanan;
-                }
-
-                $counter++;
+                $collData[$item['title']] = $getData;
             }
+
+            $fixData = [];
+            $fixData['header'][] = 'Nama Unit';
+
+
+
+            // return $collData;
+            $counter = 1;
+            foreach ($collData as $bulan => $item) {
+                $fixData['header'][] = $bulan;
+                foreach ($item as $k => $coll) {
+                    if (!isset($fixData[$coll->name])) {
+                        $fixData[$coll->name][] = $coll->name;
+                        $fixData[$coll->name][] = $coll->total_layanan;
+                    } else {
+                        $fixData[$coll->name][] = $coll->total_layanan;
+                    }
+
+                    $counter++;
+                }
+            }
+
+            $totalperBulan = [];
+            $totalperBulan[] = 'Total';
+            foreach ($collData as $bulan => $item) {
+                $totalperBulan[] = array_sum(array_column($item, 'total_layanan'));
+            }
+
+            $fixData['Total'] = $totalperBulan;
+            // return $fixData;
         }
 
-        $totalperBulan = [];
-        $totalperBulan[] = 'Total';
-        foreach ($collData as $bulan => $item) {
-            $totalperBulan[] = array_sum(array_column($item, 'total_layanan'));
+        // Disposisi
+        $undoneDisp = [];
+        if ($type != 'MINIMAL') {
+            $disposisiNotDone = \App\Models\DaftarDisposisi::whereDoesntHave('child')
+                        ->whereNotNull('id_aksi_disposisi')
+                        ->whereNotNull('id_recipient')
+                        ->with('recipient')
+                        ->get();
+
+
+            $grouped =  $disposisiNotDone->groupBy('recipient.name');
+
+            $res = [];
+            foreach ($grouped as $key => $value) {
+                $res[] = [
+                    'name' => $key,
+                    'count' => count($value)
+                ];
+            }
+
+            $undoneDisp = $res;
         }
-
-        $fixData['Total'] = $totalperBulan;
-        // return $fixData;
-
-        // DATE_FORMAT(a.created_at,'%M %Y') as bulan
-
-
-        // $dataPelayananColl = collect($collLayanan)->groupBy('unit');
-
-
-        // end of Coba-coba
-
-
-        $disposisiNotDone = \App\Models\DaftarDisposisi::whereDoesntHave('child')
-                    ->whereNotNull('id_aksi_disposisi')
-                    ->whereNotNull('id_recipient')
-                    ->with('recipient')
-                    ->get();
-
-
-        $grouped =  $disposisiNotDone->groupBy('recipient.name');
-
-        $res = [];
-        foreach ($grouped as $key => $value) {
-            $res[] = [
-                'name' => $key,
-                'count' => count($value)
-            ];
-        }
-
-        $undoneDisp = $res;
 
         return view('admin.home.index', [
             'title'  => 'Halaman Beranda',
@@ -286,7 +289,8 @@ class HomeController extends Controller
             'dataDaily'  => $dataDaily,
             'daftarpelayanangrouped'  => $daftarpelayanangrouped,
             'fixData'  => $fixData,
-            'greeting' => $this->_getGreeting()
+            'greeting' => $this->_getGreeting(),
+            'type' => $type,
         ]);
     }
 
