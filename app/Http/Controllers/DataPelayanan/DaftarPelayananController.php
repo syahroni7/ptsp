8c12e7df4d140866b82e650401324a145878b072
pramana yuda <pramanayuda772@gmail.com>
2025-03-25 14:09:02 +0700

    push pelayanan

--- app/Http/Controllers/DataPelayanan/DaftarPelayananController.php
+++ app/Http/Controllers/DataPelayanan/DaftarPelayananController.php
@@ -105,6 +105,7 @@ class DaftarPelayananController extends Controller
                 ->addColumn('pelayanan_perihal', function ($layanan) {
                     $html = '';
                     $html .= '<span>'.$layanan->perihal.  '</span><br>';
+                    return $html;
                 })
                 ->editColumn('created_at', function ($layanan) {
                     return $layanan->created_at->format('Y-m-d H:i:s');
